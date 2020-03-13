432695515a1f3728b45723ddb14573318fd8df50
JLAT <jlat0128@gmail.com>
2020-03-13 10:25:35 -0400

    updated classes so they can work in interactive interpreter

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,27 +1,25 @@
 class Person:
-    def__init__(self, id, first_name, last_name):
-        self.id =
-        self.first_name =
-        self.last_name =
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
 
 
 
 class Acccount:
-    def__init__(self, number, type, owner, balance):
-        self.number = 
-        self.type =
-        self.owner =
-        self.balance =
-        self.Person =
+    def __init__ (self, number, type, owner, balance):
+        self.number = number
+        self.type = type
+        self.owner = owner
+        self.balance = balance
+        self.Person = Person
 
 
 class Bank:
-    def__init__(self, add_customer, add_account):
-        self.add_customer
-        self.add_account
-        self.Acccount
-        self.
-
+    def __init__(self, add_customer, add_account):
+        self.add_customer = add_customer
+        self.add_account = add_account
+ 
     # Function to deposite amount 
     def deposit(self): 
 
