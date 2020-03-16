027e687bb7904ed83b5d6283470fd7956aa585b3
JLAT <jlat0128@gmail.com>
2020-03-15 22:44:42 -0400

    Re-Formatted per Pycharms suggestions

--- persistent_small_town_teller.py
+++ persistent_small_town_teller.py
@@ -1,23 +1,11 @@
-import pickle
 import json
-from small_town_teller import Bank
+import pickle
+
 
 class PersistenceUtils:
 # create PersistenceUtils static method
-    @staticmethod
 
-    def write_pickle():
-        dbc = self.customers
-        dba = self.accounts
-        f = open(saved_accounts.pickle, 'wb')  # (wb) write-binary mode
-        pickle.dump(dba, f)
-        f.close()
-        f = open(saved_customers.pickle, 'wb')  # (wb) write-binary mode
-        pickle.dump(dbc, f)
-        f.close()
-
-
-    def load_pickle():
+def load_pickle():
         f = open(saved_accounts.pickle, 'rb')
         b = pickle.load(f)
         self.accounts = (eval(json.dumps(b)))
@@ -28,3 +16,14 @@ class PersistenceUtils:
         f.close()
     
         return self.accounts, self.customers
+
+
+def write_pickle():
+    dbc = self.customers
+    dba = self.accounts
+    f = open(saved_accounts.pickle, 'wb')  # (wb) write-binary mode
+    pickle.dump(dba, f)
+    f.close()
+    f = open(saved_customers.pickle, 'wb')  # (wb) write-binary mode
+    pickle.dump(dbc, f)
+    f.close()
